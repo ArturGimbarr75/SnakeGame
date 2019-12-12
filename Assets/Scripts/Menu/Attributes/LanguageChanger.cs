b39f1489d3fad5ff50eecd8da0c4f7c589058871
Artur <Artur.Radzivilov@stud.vgtu.lt>
2019-12-12 14:59:37 +0200

    Реализована смена языков
    Реализована смена языков

---
+++ Assets/Scripts/Menu/Attributes/LanguageChanger.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Scripts.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+namespace Assets.Scripts.Menu.Attributes
+{
+    class LanguageChanger : MonoBehaviour
+    {
+        public void ChangeLanguage()
+        {
+            UISettings.Current.language++;
+
+            if ((int)UISettings.Current.language == 3)
+                UISettings.Current.language = 0;
+        }
+    }
+}
