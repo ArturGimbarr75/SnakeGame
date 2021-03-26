a0171600cb69e8033f029531c44fe6e607ddfddd
Artur <Artur.Radzivilov@stud.vgtu.lt>
2021-03-27 01:42:43 +0200

    Added new situation

---
+++ Assets/Scripts/GameLogics/Situations/CollisionWithSnake/SnakeStops.cs
@@ -0,0 +1,19 @@
+﻿using Map;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Situations
+{
+    class SnakeStops : ICollisionWithSnake
+    {
+        public void OnCollision(PlayingMapAttributes.Snake snake, PlayingMap currentMap, PlayingMap previousMap)
+        {
+            var tailPos = previousMap.Snake.Find(x => x == snake).Cordinates.Last();
+            snake.Cordinates.Add(tailPos);
+            snake.Cordinates.RemoveAt(0);
+        }
+    }
+}
